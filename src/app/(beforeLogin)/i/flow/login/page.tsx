ce98f09c246bf7ce828a23754536838510e193d3
ianlee <ethian9227@gmail.com>
2024-03-10 10:16:35 +0900

    주석 부연 설명추가

--- src/app/(beforeLogin)/i/flow/login/page.tsx
+++ src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from "next/navigation";
 
+// page.tsx대신 route.tsx 쓰는 방법도 있다
+// next 13버젼에 나온 redirect 기능
 export default function Login() {
   redirect("i/flow/login");
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,12 @@ export default function Home() {
       <div className={styles.right}>
         <h1>지금 일어나고 있는 일</h1>
         <h2>지금 가입하세요.</h2>
+        {/* a태그를 쓰면 새로고침됨 */}
         <Link href="/i/flow/singup" className={styles.signup}>
           계정 만들기
         </Link>
         <h3>이미 트위터에 가입하셨나요?</h3>
+        {/* x에서 /login으로 이동할 때 /i/flow/login으로 리다이렉트됨 */}
         <Link href="/login" className={styles.login}>
           로그인
         </Link>
