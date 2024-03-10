dc5167915d578a7bee465a2cf11954345bc9008e
ianlee <ethian9227@gmail.com>
2024-03-10 11:40:23 +0900

    서버 리다이렉트에서 클라이언트 리다이렉트로 useRouter 수정

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,7 +1,11 @@
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 // page.tsx대신 route.tsx 쓰는 방법도 있다
-// next 13버젼에 나온 redirect 기능
+// next 13버젼에 나온 redirect 기능 (서버 리다이렉트)
+
+// 클라이언트 리다이렉트로 바꿔줘야해서 redirect 대신 useRouter로 수정
 export default function Login() {
-  redirect("i/flow/login");
+  const router = useRouter();
+  router.replace("/i/flow/login");
+  return null;
 }
